45ba1f284f31901e82a974c66b2f0667e957f792
Florian Motlik <flomotlik@gmail.com>
2017-06-23 21:14:22 +0200

    Load specification from S3 for every call

--- former/specification.py
+++ former/specification.py
@@ -1,8 +1,10 @@
 import json
 
+import requests
+
 
 def specification():
-    # https://d1uauaxba7bl26.cloudfront.net/latest/CloudFormationResourceSpecification.json
-
-    with open('CloudFormationResourceSpecification.json') as f:
-        return json.loads(f.read())
+    response = requests.get(
+        'https://d1uauaxba7bl26.cloudfront.net/latest/CloudFormationResourceSpecification.json')
+    response.raise_for_status()
+    return json.loads(response.text)
