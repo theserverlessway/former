38f98cb1c3695c284c161971e3ce8fae930cf267
Florian Motlik <flo@theserverlessway.com>
2022-01-31 12:44:54 +0100

    Remove Pathlib as a dependency

--- former/specification.py
+++ former/specification.py
@@ -1,16 +1,11 @@
 import os
 import json
 import platform
-import pathlib
+import tempfile
 
 import requests
 
-if platform.system() == 'Windows':
-    CACHE_PATH = pathlib.WindowsPath(os.getenv('TEMP', '~') + '/former-spec.cached.json').expanduser()
-else:
-    CACHE_PATH = pathlib.Path('/tmp/former-spec.cached.json')
-CACHE_PATH = str(CACHE_PATH)
-
+CACHE_PATH = tempfile.gettempdir() + '/former-spec.cached.json'
 
 def specification():
     if os.path.exists(CACHE_PATH):
